28c6c0ebc578cebecf8d890b4ab03cbb3af8cc3d
juliatduffy <jtduffy7@gmail.com>
2025-11-18 12:13:27 -0700

    minimal progress but I understand this a lot better now

--- malloclab-handout/check_size.c
+++ malloclab-handout/check_size.c
@@ -9,35 +9,12 @@
 #include "memlib.h"
 #include "pagemap.h"
 
-void *curr_block_ptr = NULL;
-#define HEADERSIZE 32
-
 typedef struct block_header { 
   size_t size;
-  struct block_header* next;
   char allocated;
-  struct block_header* prev; 
 } block_header; 
 
+
 int main() {
-    printf("Size of block header: %zu bytes\n", sizeof(block_header));
-
-    int size = 4;
-    int newsize = ALIGN(size);
-    void * new_block = mmap(0, APAGE_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON, -1, 0);;
-    curr_block_ptr  += newsize + HEADERSIZE;
-    
-    // make header for new block
-    block_header * curr_header = (block_header *) curr_block_ptr;
-    curr_header->size = size;
-    curr_header->allocated = 1;
-    curr_header->prev = prev;
-
-    // return pointer to new block (starting after header)
-    return curr_block_ptr;
-    
-    printf("Address of prev header: %d\n", &prev_header);
-    
-    printf("Address of curr header: %d\n", &curr_header);
-    return 0;
+    printf("Size of node: %zu bytes\n", sizeof(block_header));
 }
--- malloclab-handout/mm.c
+++ malloclab-handout/mm.c
@@ -16,74 +16,77 @@
 #include "mm.h"
 #include "memlib.h"
 
-/* always use 16-byte alignment */
-#define ALIGNMENT 16
+#define OVERHEAD (sizeof(block_header)+sizeof(block_footer)) // calculate overhead
+#define HDRP(bp) ((char *)(bp) - sizeof(block_header)) // given bp, get the header or footer pointer
+#define FTRP(bp) ((char *)(bp)+GET_SIZE(HDRP(bp))-OVERHEAD) // given bp, get the footer pointer
+#define NEXT_BLKP(bp) ((char *)(bp) + GET_SIZE(HDRP(bp))) // get the next payload pointer
+#define PREV_BLKP(bp) ((char *)(bp)-GET_SIZE((char *)(bp)-OVERHEAD)) // get the previous payload pointer
 
-/* rounds up to the nearest multiple of ALIGNMENT */
-#define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~(ALIGNMENT-1))
-
-/* rounds up to the nearest multiple of mem_pagesize() */
-#define PAGE_ALIGN(size) (((size) + (mem_pagesize()-1)) & ~(mem_pagesize()-1))
-
-#define HEADERSIZE 32 // yuck lol
-
-void *curr_block_ptr = NULL;
-int curr_block_size = HEADERSIZE;
+// ******These macros assume you are using a size_t for headers and footers ******
+#define GET(p) (*(size_t *)(p))// get value at pointer 
+#define PUT(p, val) (*(size_t *)(p) = (val)) // set value at pointer 
+#define PACK(size, alloc) ((size) | (alloc)) // Combine a size and alloc bit
+#define GET_ALLOC(p) (GET(p) & 0x1) // Given a header pointer get the allocation 
+#define GET_SIZE(p) (GET(p) & ~0xF) // Given a header pointer get the size 
+#define ALIGNMENT 16 // always use 16-byte alignment
+#define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~(ALIGNMENT-1)) // rounds up to the nearest multiple of ALIGNMENT
+#define PAGE_ALIGN(size) (((size) + (mem_pagesize()-1)) & ~(mem_pagesize()-1)) // rounds up to the nearest multiple of mem_pagesize()
 
 typedef struct block_header { 
   size_t size;
-  struct block_header* next;
   char allocated;
-  struct block_header* prev; 
-} block_header;  // 32 bytes total
+} block_header; 
+
+typedef struct node {
+  size_t value; // pointer to THIS memory
+  size_t next; 
+} node; 
+
+// make prolog and epilog
+
+#define HEADERSIZE sizeof(block_header) // 16 bytes 
+#define NODESIZE sizeof(node)  // 16 bytes
+
+node * first_node = {NULL, NULL}; // stores head of free linked list, ok to have explicit free list
+int current_avail_size = 0;
 
 /* 
- * mm_init - initialize the malloc package.
+ * mm_init - initialize the malloc package. NOt 100% sure what to do here
  */
 int mm_init(void)
 {
-  // ptr to most recently allocated block
-  curr_block_ptr = NULL;
-  // size of most recently allocated block
-  curr_block_size = 0;
+  // create free list
+  // create empty free list 
+  // extend the heap to create an initial free block
+  first_node-> value = mem_map(4 * __WORDSIZE); // not sure where this wordsize thing came from
+  first_node -> next = NULL;
+  int current_avail_size = 0;
+
   return 0;
 }
 
 /* 
  * mm_malloc - Allocate a block by using bytes from new_block,
- *     grabbing a new page if necessary.
+ *     grabbing a new page if necessary. need to split big block
  */
 void *mm_malloc(size_t size)
 {
-  size += HEADERSIZE;
+  size += HEADERSIZE; 
   int newsize = ALIGN(size);
-  void * prev = curr_block_ptr;
 
-  // if we need more space call mem_map to get a new page
-  if (curr_block_size + HEADERSIZE < newsize) {
-    curr_block_size = PAGE_ALIGN(newsize);
-    void * new_block = mem_map(curr_block_size); // NEW PAGE
-    if (new_block == NULL) {
+  if (current_avail_size < newsize) {
+    current_avail_size = PAGE_ALIGN(newsize);
+    current_avail = mem_map(current_avail_size);
+    if (current_avail == NULL)
       return NULL;
-    }
-    curr_block_ptr = new_block;
-    }
-
-  curr_block_ptr  += newsize;
-  curr_block_size -= newsize; // don't understand this
+  }
   
   // make header for new block
   block_header * curr_header = (block_header *) curr_block_ptr;
   curr_header->size = size;
   curr_header->allocated = 1;
-  curr_header->prev = prev;
-  curr_header->next = NULL;
  
-  // set "next" pointer for previous block
-  if (prev != NULL) {
-    block_header* prev_header = (block_header *) prev;
-    prev_header-> next = curr_header;
-  }
+  // update free list
 
   // return pointer to new block (starting after header)
   return (void *) (curr_header + 1);// question: +1 gives headersize more bytes right? or 1 word?
@@ -97,3 +100,13 @@ void mm_free(void *ptr)
   // block_header * header = (block_header *)ptr;
   // header -> allocated = 0;
 }
+
+void extend(size_t s) {
+// if we need more space call mem_map to get a new page
+  free_list_tail = PAGE_ALIGN(s);
+  void * new_block = mem_map(free_list_tail); // NEW PAGE
+  if (new_block == NULL) {
+    return NULL;
+  }
+  free_list_tail = new_block;
+  }
